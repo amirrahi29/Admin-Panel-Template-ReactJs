e85d3a6d21a5c28428852a503d59294eca250b4d
Amir Rahi <amirrahi29@gmail.com>
2023-06-18 17:36:41 +0530

    added

--- src/views/stores/ViewStore.js
+++ src/views/stores/ViewStore.js
@@ -1,18 +1,17 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import DataTable from 'react-data-table-component'
+import React, { useEffect, useState } from 'react';
+import DataTable from 'react-data-table-component';
 
 const ViewStore = () => {
-
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [search, setSearch] = useState("");
+  const [selectedRows, setSelectedRows] = useState([]);
 
   useEffect(() => {
     getAllProducts();
   }, []);
 
-  //search
   useEffect(() => {
     const result = products.filter(product => {
       return product.title.toLowerCase().match(search.toLowerCase());
@@ -26,6 +25,10 @@ const ViewStore = () => {
     setFilteredProducts(response.data);
   }
 
+  const handleSelectedRowsChange = (state) => {
+    setSelectedRows(state.selectedRows);
+  };
+
   const columns = [
     {
       name: "ID",
@@ -34,7 +37,7 @@ const ViewStore = () => {
     },
     {
       name: "IMAGE",
-      selector: (row) => <img width={60} height={60} src={row.image} />
+      selector: (row) => <img width={60} height={60} src={row.image} alt={row.title} />
     },
     {
       name: "TITLE",
@@ -47,37 +50,66 @@ const ViewStore = () => {
     },
     {
       name: "DELETE",
-      cell: (row) => <button className='btn btn-danger' onClick={() => alert(row.title)}>Delete</button>
+      cell: (row) => <button className='btn btn-danger' style={{ color: 'white' }} onClick={() => alert(row.title)}>Delete</button>
     }
-  ]
+  ];
+
+  const conditionalRowStyles = [
+    {
+      when: (row) => row.title === "Mens Casual Slim Fit",
+      style: {
+        backgroundColor: 'grey',
+        color: 'white',
+      },
+    },
+  ];
 
   return (
-    <DataTable
-      title='All Stores'
-      data={filteredProducts}
-      columns={columns}
-      pagination
-      fixedHeader
-      fixedHeaderScrollHeight='450px'
-      selectableRows
-      selectableRowsHighlight
-      highlightOnHover
-      actions={
-        <button className="btn btn-sm btn-info">Export</button>
-      }
-      subHeader
-      subHeaderAlign='left'
-      subHeaderComponent={
-        <input type='text'
-          value={search}
-          placeholder='Search here'
-          className='w-25 form-control'
-          onChange={(e) => setSearch(e.target.value)}
-        />
-      }
+    <div>
+      {selectedRows.length > 0 ?
+        <p style={{ marginLeft: 20, marginBottom: 0 }}>
+          SELECTED ITEMS<hr style={{ margin: 0, padding: 0 }} />
+        </p>
+        : null}
+      <ul>
+        {selectedRows.length > 0 ?
+          <>
+            {selectedRows.map(row => (
+              <li key={row.id}>{row.title}</li>
+            ))}
+          </>
+          : null}
+      </ul>
 
-    />
+      <DataTable
+        title='All Stores'
+        data={filteredProducts}
+        columns={columns}
+        pagination
+        fixedHeader
+        fixedHeaderScrollHeight='450px'
+        selectableRows
+        selectableRowsHighlight
+        highlightOnHover
+        actions={
+          <button className="btn btn-sm btn-info">Export</button>
+        }
+        subHeader
+        subHeaderAlign='left'
+        subHeaderComponent={
+          <input type='text'
+            value={search}
+            placeholder='Search here'
+            className='w-25 form-control'
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        }
+        conditionalRowStyles={conditionalRowStyles}
+        onSelectedRowsChange={handleSelectedRowsChange}
+      />
+
+    </div>
   )
 }
 
-export default ViewStore
+export default ViewStore;
